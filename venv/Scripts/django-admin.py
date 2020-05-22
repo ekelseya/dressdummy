28a0586a484c15e6c793843187f7c13676d7e6d6
ekelseya <42783392+ekelseya@users.noreply.github.com>
2020-05-22 15:30:58 -0600

    Directory updates

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\eryn kelsey-adkins\documents\github\dummy\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
